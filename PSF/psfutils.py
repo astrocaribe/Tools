25f0e2ffc775c597ced34b10ebd266185fbd1fbe
astrocaribe <leblanc@stsci.edu>
2013-11-11 12:55:26 -0500

    PSF utility file

---
+++ PSF/psfutils.py
@@ -0,0 +1,23 @@
+# Import necessary modules
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import pyfits as pf
+
+import logging
+import pywcs
+
+# Header
+__author__ = "Tommy Le Blanc"
+__version__ = "1.0"
+
+# HISTORY
+#    1. Jan 2013 - Vr. 1.0: Added initial PSF tools
+#                          - 
+#                          - 
+
+
+
+
+# Print diagnostic load message
+print("(psfutils): PSF Utilities Version {} loaded!".format(__version__))
