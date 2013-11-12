e3b909c48d13548f53d75713de90ce77f094dbb0
astrocaribe <leblanc@stsci.edu>
2013-11-12 12:02:15 -0500

    Added gen_central_thrumap function

--- PSF/psfutils.py
+++ PSF/psfutils.py
@@ -15,8 +15,8 @@ __version__ = "1.0"
 # HISTORY
 #    1. Nov 2013 - Vr. 1.0: Added initial PSF tools
 #                          - Added mask_psf function
-#                          - Added gen_2d_thrumap function
-#                          - Added gen_3d_thrumap function
+#                          - Added gen_central_thrumap function
+#                          - Added gen_3x3_thrumap function
 
 
 # Utility definitions
@@ -50,6 +50,11 @@ def mask_image(x, y, c, image, verbose=False):
         given an x, y MSA position of (0, 0) and a PSF centered at center, and an 
         initial image psf.
     """
+
+    # MSA shutter width, height, and grid width
+    # Dimensions are for 10X oversampled pixels, each = 0.01"
+    dx, dy, gridwidth = 20, 40, 6
+
 
     # Locate pointing center based on input x and y centering parameters
     # (for MSA grid mask)
@@ -131,6 +136,78 @@ def mask_image(x, y, c, image, verbose=False):
             
     return masked_image, shutter_flux
 # *********************** mask_psf ***********************
+
+
+# *********************** gen_central_thruput ***********************
+def gen_central_thrumap(infile, wavelength, lvls, s_path):
+    """
+    Generate a throughput map of the central shutter (from a 5x5 config).
+
+    This function generate a throughput map (percentage of flux) in a 
+    central shutter (of a 5x5 shutter configuration), given an input 
+    datacube. This version assumes a shape (441, 5, 5), corresponding to 
+    21 x and y dithers of a psf within the central shutter.
+
+    Keyword arguments:
+    x, y    -- Central MSA pointing coordinates
+    c       -- Input PSF centroid coordinates
+    image   -- Input PSF image
+    verbose -- Toggle verbose mode. (default False).
+
+    Output(s):
+    masked_image -- The masked PSF
+    shutter_flux -- The flux values, summed per 5 x 5 shutter
+
+    Example use:
+
+        >>    masked_psf, psf_shutter_flux = mask_image(0.0, 0.0, center, psf)
+
+        Generates a masked psf image along with a 5 x 5 summed shutter_flux array
+        given an x, y MSA position of (0, 0) and a PSF centered at center, and an 
+        initial image psf.
+
+    """
+    
+    # Read in throughput cube
+    hdu = pf.open(infile)
+    steps = hdu[0].header['NSTEP']
+
+    cube = hdu[1].data.astype(np.float64)
+    #print(cube.shape)
+    #print()
+
+    # Reshape the central shutter values into 2D
+    x = cube[:, 2, 2].reshape(steps,steps)
+
+    # 2D representation
+    fig, ax = plt.subplots(figsize=(5,8))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('MSA Shutter Throughput Map ({} $\mu m$)'.format(wavelength))
+
+    cmap = cm.get_cmap('jet', 20)    # 20 discrete colors
+
+    cax = plt.contourf(x, cmap=cmap, levels=lvls, antialiased=False, vmin=0.15, vmax=0.7, alpha=0.8)
+    plt.axes().set_aspect(2.)
+
+    # Find and plot throughput max
+    t_max = np.where(x == x.max().astype(np.float64))
+    max_x, max_y = t_max[1][0], t_max[0][0]
+    #print('Max point of {} at [{}, {}]'.format(x.max(), max_x, max_y))
+    ax = plt.plot(max_x, max_y, marker='+', ms=10, mfc='black', mec='black')
+
+    cnt = plt.contour(x, colors='black', linewidths=.5, levels=lvls)#, vmin=abs(x).min(), vmax=abs(x).max())
+    plt.clabel(cnt, fontsize=8)
+
+    #fig.savefig(s_path+'/ThruMap_{}.pdf'.format(wavelengths[psf_file]))
+    fig.savefig(s_path+'/Images2D/ThruMap_{}.jpg'.format(wavelength))
+    
+    print(r"MSA Shutter Throughput Map ({} $\\mu$ m) done ...".format(wavelength))
+
+    hdu.close()
+    
+    return fig
+# *********************** gen_central_thruput ***********************
 
 
 
